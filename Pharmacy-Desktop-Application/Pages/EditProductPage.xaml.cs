995713fba5e2873a57fb81415fb731a6fe0ae39f
JarJarSyncs <jettcurrie@outlook.com>
2021-09-25 17:25:01 +1000

    Fixed Display Sales linking in solution

---
+++ Pharmacy-Desktop-Application/Pages/EditProductPage.xaml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pharmacy_Desktop_Application
+{
+    /// <summary>
+    /// Interaction logic for EditProductPage.xaml
+    /// </summary>
+    public partial class EditProductPage : Page
+    {
+        //TODO Currently this page is only setup to add product to the database
+
+        public EditProductPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
