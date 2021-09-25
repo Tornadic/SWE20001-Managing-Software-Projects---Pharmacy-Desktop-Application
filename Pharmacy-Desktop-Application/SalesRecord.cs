c3953e8f46c7911f29928640f566c79bf0777370
JarJarSyncs <jettcurrie@outlook.com>
2021-09-25 16:46:32 +1000

    sales record object created (can change to struct later if it doesn't need any functions)

--- Pharmacy-Desktop-Application/SalesRecord.cs
+++ Pharmacy-Desktop-Application/SalesRecord.cs
@@ -8,19 +8,40 @@ namespace Pharmacy_Desktop_Application
 {
     class SalesRecord
     {
-        public SalesRecord() 
-        {
+        //PHPSalesRecord
+        private int _salesRecID;
+        private DateTime _salesRecDate;
+        private float _salesRecTotal;
 
+        //PHPItemSales
+        private int _itmSaleID;
+        private string _itmSaleProdSKU;
+        private int _itmSaleQuantity;
+        private int _itmSaleRecordID;
+
+        public SalesRecord(int salesRecID, DateTime salesRecDate, float salesRecTotal, int itmSaleID, string itmSaleProdSKU, int itmSaleQuantity, int itmSaleRecordID)
+        {
+            //PHPSalesRecord
+            _salesRecID = salesRecID;
+            _salesRecDate = salesRecDate;
+            _salesRecTotal = salesRecTotal;
+
+            //PHPItemSales
+            _itmSaleID = itmSaleID;
+            _itmSaleProdSKU = itmSaleProdSKU;
+            _itmSaleQuantity = itmSaleQuantity;
+            _itmSaleRecordID = itmSaleRecordID;
         }
 
-        public void AddSR()
-        {
+        //PHPSalesRecord
+        public int SalesRecID { get => _salesRecID; }
+        public DateTime SalesRecDate { get => _salesRecDate; }
+        public float SalesRecTotal { get => _salesRecTotal; }
 
-        }
-
-        public void EditSR()
-        {
-
-        }
+        //PHPItemSales
+        public int ItmSaleID { get => _itmSaleID; }
+        public string ItmSaleProdSKU { get => _itmSaleProdSKU; }
+        public int ItmSaleQuantity { get => _itmSaleQuantity; }
+        public int ItmSaleRecordID { get => _itmSaleRecordID; }
     }
 }
