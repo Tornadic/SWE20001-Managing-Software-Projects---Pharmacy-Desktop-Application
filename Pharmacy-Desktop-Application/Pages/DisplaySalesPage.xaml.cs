e1c094068f7269b8712a4ce96cfd125e4e95f890
callumbedson <cjbedson@gmail.com>
2021-09-27 15:24:47 +1000

    Added CreateCSV method

--- Pharmacy-Desktop-Application/Pages/DisplaySalesPage.xaml.cs
+++ Pharmacy-Desktop-Application/Pages/DisplaySalesPage.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -56,6 +57,53 @@ namespace Pharmacy_Desktop_Application
         private void BtnClickAddSalesRecord(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new SalesPage());
+        }
+
+        private void CreateCSV()
+        {
+            InitializeComponent();
+            var runSQL = SQLobj.Instance();
+            List<List<string>> phpSalesRecord = runSQL.QueryDB("SELECT * from PHPSalesRecord WHERE salesRecDate BETWEEN (CURRENT_DATE() - INTERVAL 1 MONTH) AND CURRENT_DATE()");
+            List<List<string>> phpItemSales = runSQL.QueryDB("SELECT * from PHPItemSales"); //Get last month only?
+
+            List<SalesRecord> csvSalesRecord = new List<SalesRecord>();
+
+            if (phpSalesRecord is null || phpItemSales is null)
+            {
+                Console.WriteLine("Query returned null");
+            }
+            else
+            {
+                foreach (List<string> salesRec in phpSalesRecord)
+                {
+                    List<ItemSale> itmSales = new List<ItemSale>();
+                    foreach (List<string> itmSale in phpItemSales)
+                    {
+                        itmSales.Add(new ItemSale(Convert.ToInt32(itmSale[0]), itmSale[1], Convert.ToInt32(itmSale[2])));
+                    }
+                    csvSalesRecord.Add(new SalesRecord(Convert.ToInt32(salesRec[0]), Convert.ToDateTime(salesRec[1]), float.Parse(salesRec[2]), itmSales));
+                }
+                try
+                {
+                    //To find path: string reportPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments etc);, System.IO.Path.Combine(), Permissions might need to be granted
+                    //How to title sales report? Maybe date
+                    StreamWriter fileOutput = new StreamWriter("C:\\SalesReports\\SalesReport.csv");
+                    foreach (SalesRecord sr in csvSalesRecord)
+                    {
+                        string itemsSold = "";
+                        foreach (ItemSale itemSale in sr.ItemSales)
+                        {
+                            itemsSold += ", " + itemSale.ItmSaleID + ", " + itemSale.ItmSaleProdSKU + ", " + itemSale.ItmSaleQuantity;
+                        }
+                        fileOutput.WriteLine(sr.SalesRecID + ", " + sr.SalesRecDate.ToString("dd-MM-yyyy") + ", " + sr.SalesRecPrice + itemsSold);
+                    }
+                    fileOutput.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+            }
         }
     }
 }
