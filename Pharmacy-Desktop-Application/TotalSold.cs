bb1332073a1aaa687f5e594e37351ca86673382e
dtquyen1199 <88473863+dtquyen1199@users.noreply.github.com>
2021-09-28 10:27:29 +1000

    Add files via upload

---
+++ Pharmacy-Desktop-Application/TotalSold.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Desktop_Application
+{
+    public class TotalSoldByItem
+    {
+        public TotalSoldByItem(string prodSKU, int totalQuantity)
+        {
+            SKU = prodSKU;
+            TotalQuantity = totalQuantity;
+        }
+
+        public string SKU { get; set; }
+        public int TotalQuantity { get; set; }
+    }
+}
