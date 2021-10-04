f1fd3c4a98324c8a5673a2dc54f13299462c575c
THIEM QUYEN <102436746@student.swin.edu.au>
2021-10-04 21:13:13 +1100

    Added product combo prediction

---
+++ Pharmacy-Desktop-Application/ComboSold.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Desktop_Application
+{
+    public class ComboSold
+    {
+        public ComboSold(List<string> prodsSKU, int occurrence)
+        {
+            ProdsSKU = prodsSKU;
+            Occurrence  = occurrence;
+        }
+        public List<string> ProdsSKU { get; set; }
+        public int Occurrence { get; set; } = 0;
+    }
+}
--- Pharmacy-Desktop-Application/Pages/PredictionPage.xaml.cs
+++ Pharmacy-Desktop-Application/Pages/PredictionPage.xaml.cs
@@ -10,6 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Pharmacy_Desktop_Application
 {
@@ -27,10 +30,12 @@ namespace Pharmacy_Desktop_Application
             InitializeComponent();
         }
 
-        public List<TotalSoldByItem> MatchAndSortByItem(List<List<string>> salesRecord, List<List<string>> itemSales )
+
+
+        public List<SalesRecord> MatchItemSaleToSalesRecord(List<List<string>> salesRecord, List<List<string>> itemSales)
         {
             List<SalesRecord> phpSalesRecord = new List<SalesRecord>();
-            List<TotalSoldByItem> listOfSoldItems = new List<TotalSoldByItem>();
+            
             foreach (List<string> salesRec in salesRecord)
             {
                 List<ItemSale> itmSales = new List<ItemSale>();
@@ -43,7 +48,13 @@ namespace Pharmacy_Desktop_Application
                 }
                 phpSalesRecord.Add(new SalesRecord(Convert.ToInt32(salesRec[0]), Convert.ToDateTime(salesRec[1]), float.Parse(salesRec[2]), itmSales)); // This is where I can work from and chop of months/weeks/ quarterrs?
             }
+            return phpSalesRecord;
+        }
 
+        public List<TotalSoldByItem> MatchAndSortByItem(List<List<string>> salesRecord, List<List<string>> itemSales )
+        {
+            List<TotalSoldByItem> listOfSoldItems = new List<TotalSoldByItem>();
+            List<SalesRecord> phpSalesRecord = MatchItemSaleToSalesRecord(salesRecord,itemSales); 
             foreach (SalesRecord sr in phpSalesRecord)
             {
                 foreach (ItemSale itemSale in sr.ItemSales)
@@ -60,17 +71,106 @@ namespace Pharmacy_Desktop_Application
             }
             return listOfSoldItems;
         }
+
+
+        private static IEnumerable<IEnumerable<T>> GetCombinations<T>(IEnumerable<T> list, int length) where T : IComparable
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetCombinations(list, length - 1).SelectMany(t => list.Where(o => o.CompareTo(t.Last()) > 0), (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
+        public List<List<string>> Pair(List<string> list)
+        {
+            IEnumerable<IEnumerable<string>> enumResult = GetCombinations(list, 2);
+            //
+            List<List<string>> listResult = new List<List<string>>();
+            foreach (IEnumerable<string> pair in enumResult)
+            {
+                List<string> pair2 = pair.ToList();
+                pair2.Sort();
+                listResult.Add(pair2);
+            }
+            return listResult;
+
+        }
+
+        public List<ComboSold> MatchAndSortByPair(List<List<string>> salesRecord, List<List<string>> itemSales)
+        {
+            List<SalesRecord> phpSalesRecord = MatchItemSaleToSalesRecord(salesRecord, itemSales);
+            List<List<string>> purchasedComboList = new List<List<string>>();
+            List<ComboSold> comboSoldRecord = new List<ComboSold>();
+            foreach (SalesRecord sr in phpSalesRecord)
+            {
+                List<string> purchasedSKUs = new List<string>();
+                foreach (ItemSale itmSale in sr.ItemSales)
+                {
+                    //add to the pool?
+                    purchasedSKUs.Add(itmSale.ItmSaleProdSKU);
+                }
+                purchasedComboList.AddRange(Pair(purchasedSKUs));
+            }
+
+            List<List<string>> distinct = purchasedComboList.Distinct().ToList();
+            foreach (List<string> pair in distinct)
+            {
+                int frequency = purchasedComboList.Count(p => (p[0] == pair[0]) && (p[1] == pair[1]));
+                comboSoldRecord.Add(new ComboSold(pair, frequency));
+            }
+            return comboSoldRecord;
+        }
+
+
+        private void BtnClickWeekCombo(object sender, RoutedEventArgs e)
+        {
+            var runSQL = SQLobj.Instance();
+            List<List<string>> phpItemSales = runSQL.QueryDB("SELECT * from PHPItemSales");
+            List<List<string>> phpWeeklySalesRecord = runSQL.QueryDB("SELECT * from PHPSalesRecord WHERE salesRecDate BETWEEN (CURRENT_DATE() - INTERVAL 7 DAY) AND CURRENT_DATE()");
+            textComboPredictionList.Text = "Product combo from last week";
+            if (phpWeeklySalesRecord.Count > 0)
+            {
+                List<ComboSold> comboSoldRec = MatchAndSortByPair(phpWeeklySalesRecord, phpItemSales);
+                foreach (ComboSold combo in comboSoldRec)
+                {
+                    textComboPredictionList.Text += $"\n{string.Join(", ", combo.ProdsSKU)} {combo.Occurrence}";
+                }
+            }
+            else { textComboPredictionList.Text += "No recent data"; }
+        }
+
+        private void BtnClickMonthCombo(object sender, RoutedEventArgs e)
+        {
+            var runSQL = SQLobj.Instance();
+            List<List<string>> phpItemSales = runSQL.QueryDB("SELECT * from PHPItemSales");
+            List<List<string>> phpMonthlySalesRecord = runSQL.QueryDB("SELECT * from PHPSalesRecord WHERE salesRecDate BETWEEN (CURRENT_DATE() - INTERVAL 1 MONTH) AND CURRENT_DATE()");
+            textComboPredictionList.Text = "Product combo from last month";
+            if (phpMonthlySalesRecord.Count > 0)
+            {
+                List<ComboSold> comboSoldRec = MatchAndSortByPair(phpMonthlySalesRecord, phpItemSales);
+                foreach (ComboSold combo in comboSoldRec)
+                {
+                    textComboPredictionList.Text += $"\n{string.Join(", ", combo.ProdsSKU)} {combo.Occurrence}";
+                }
+            }
+            else { textComboPredictionList.Text += "No recent data"; }
+        }
+
+
+
         private void BtnClickWeek(object sender, RoutedEventArgs e)
         {
             var runSQL = SQLobj.Instance();
             List<List<string>> phpItemSales = runSQL.QueryDB("SELECT * from PHPItemSales");
             List<List<string>> phpWeeklySalesRecord = runSQL.QueryDB("SELECT * from PHPSalesRecord WHERE salesRecDate BETWEEN (CURRENT_DATE() - INTERVAL 7 DAY) AND CURRENT_DATE()");
-            _listOfSoldItems = MatchAndSortByItem(phpWeeklySalesRecord, phpItemSales);
-            foreach (TotalSoldByItem soldItem in _listOfSoldItems)
+            textPredictionList.Text = "Forecasted next week sales in units: ";
+            if (phpWeeklySalesRecord.Count > 0)
             {
-
-                textPredictionList.Items.Add($"{soldItem.SKU}" + ": will be selling " + $"{soldItem.TotalQuantity}");
+                _listOfSoldItems = MatchAndSortByItem(phpWeeklySalesRecord, phpItemSales);
+                foreach (TotalSoldByItem soldItem in _listOfSoldItems)
+                {
+                    textPredictionList.Text += $"\n{soldItem.SKU} {soldItem.TotalQuantity}";
+                }
             }
+            else { textPredictionList.Text += "No recent data to make weekly predictions"; }
 
         }
 
@@ -79,12 +179,17 @@ namespace Pharmacy_Desktop_Application
             var runSQL = SQLobj.Instance();
             List<List<string>> phpItemSales = runSQL.QueryDB("SELECT * from PHPItemSales");
             List<List<string>> phpMonthlySalesRecord = runSQL.QueryDB("SELECT * from PHPSalesRecord WHERE salesRecDate BETWEEN (CURRENT_DATE() - INTERVAL 1 MONTH) AND CURRENT_DATE()");
-            _listOfSoldItems = MatchAndSortByItem(phpMonthlySalesRecord, phpItemSales);
-            foreach (TotalSoldByItem soldItem in _listOfSoldItems)
+            textPredictionList.Text = "Forecasted next month sales in units: ";
+            if (phpMonthlySalesRecord.Count > 0)
             {
-                textPredictionList.Items.Add($"Forecasted next month sales in units: {soldItem.TotalQuantity}");
+                _listOfSoldItems = MatchAndSortByItem(phpMonthlySalesRecord, phpItemSales);
+                
+                foreach (TotalSoldByItem soldItem in _listOfSoldItems)
+                {
+                    textPredictionList.Text += $"\n{soldItem.SKU} {soldItem.TotalQuantity}";
+                }
             }
-
+            else { textPredictionList.Text += "\nNo recent data to make monthly predictions"; }
         }
     }
 }
