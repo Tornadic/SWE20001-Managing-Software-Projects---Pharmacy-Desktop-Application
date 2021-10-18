38a50567e898c358136d9bdb3e63243e5dbf7c09
JarJarSyncs <jettcurrie@outlook.com>
2021-10-18 21:33:32 +1100

    Created initial notification and low stock page. Once settings page is complete can add notification option.

--- Pharmacy-Desktop-Application/App.xaml.cs
+++ Pharmacy-Desktop-Application/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace Pharmacy_Desktop_Application
 {
@@ -14,5 +15,27 @@ namespace Pharmacy_Desktop_Application
     /// dan is smelly, but in a good way 
     public partial class App : Application
     {
+        //Opens low stock page when Toast notification is clicked
+        protected override void OnActivated(EventArgs e)
+        {
+            // Listen to notification activation
+            ToastNotificationManagerCompat.OnActivated += toastArgs =>
+            {
+                // Obtain the arguments from the notification
+                ToastArguments args = ToastArguments.Parse(toastArgs.Argument);
+
+                // Need to dispatch to UI thread if performing UI operations
+                Application.Current.Dispatcher.Invoke(delegate
+                {
+                    ((MainWindow)System.Windows.Application.Current.MainWindow).Main.Content = new LowStockPage();
+                    // TODO: Show the corresponding content
+                    //MessageBox.Show("Toast activated. Args: " + toastArgs.Argument);
+                });
+            };
+            // TODO: Show the corresponding content
+            
+
+
+        }
     }
 }
--- Pharmacy-Desktop-Application/Pages/HomePage.xaml.cs
+++ Pharmacy-Desktop-Application/Pages/HomePage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace Pharmacy_Desktop_Application
 {
@@ -18,9 +19,32 @@ namespace Pharmacy_Desktop_Application
     /// </summary>
     public partial class HomePage : Page
     {
+        private int lowStock = 0;
+
         public HomePage()
         {
             InitializeComponent();
+
+            //Notification for low stock
+            var runSQL = SQLobj.Instance();
+            List<List<string>> products = runSQL.QueryDB("SELECT * from PHPProducts");
+            foreach (List<string> product in products)
+            {
+                if (int.Parse(product[2]) < 100)
+                    lowStock += 1;
+            }
+
+            if (lowStock > 0)
+            {
+                string notifs = lowStock > 1 ? string.Format("You have {0} products low on stock", lowStock) : "You have 1 product low on stock";
+                new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText(notifs)
+                .AddText("Click here to see products.")
+                .AddAppLogoOverride(new Uri("https://picsum.photos/48?image=883"))
+                .Show();
+            }
         }
     }
 }
